e4a2c50a16f2e3281470994778f21bbce8e33e5f
VedicXcel Innovations <drona.gaming@abhishektarafdar.in>
2024-11-13 22:12:16 +0530

    Update style.js

--- src/style.js
+++ src/style.js
@@ -3,13 +3,10 @@ const styles = {
 
   heading1:
     "font-poppins font-semibold xs:text-[30px] text-[25px] text-white xs:leading-[57.6px] leading-[50.1px] w-full",
-
   heading2:
     "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
-
   heading3:
     "font-poppins font-semibold xs:text-[24px] text-[20px] text-white xs:leading-[46.08px] leading-[40.8px] w-full",
-
   heading4:
     "font-poppins font-semibold xs:text-[30px] text-[25px] xs:leading-[57.6px] leading-[50.1px] w-full text-center",
 
@@ -30,10 +27,8 @@ const styles = {
 export const layout = {
   section: `flex md:flex-row flex-col ${styles.paddingY}`,
   sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
-
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
   sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
-
   sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };
 
